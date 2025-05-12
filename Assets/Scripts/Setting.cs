07265223e007182c9306e7af091197196f2510fa
Artori4z <kongphop.buch@bumail.net>
2025-05-12 22:23:29 +0700

    color

--- Assets/Scripts/Setting.cs
+++ Assets/Scripts/Setting.cs
@@ -1,32 +1,70 @@
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-
+public enum PlayerColor
+{
+    Red,
+    Green,
+    Blue,
+    Yellow
+}
 public class Setting : MonoBehaviour
 {
     public static Setting Instance;
-    bool isSoundOn = true;
-    void Awake()
+
+     PlayerColor p1Color = PlayerColor.Red;
+     PlayerColor p2Color = PlayerColor.Blue;
+
+    public TMP_Dropdown p1Dropdown;
+    public TMP_Dropdown p2Dropdown;
+
+    private void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
+        if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        if (p1Dropdown != null)
+        {
+            p1Dropdown.value = (int)p1Color;
+            p1Dropdown.onValueChanged.AddListener(SetP1Color);
+        }
+        if (p2Dropdown != null)
+        {
+            p2Dropdown.value = (int)p2Color;
+            p2Dropdown.onValueChanged.AddListener(SetP2Color);
         }
     }
-    public Toggle soundToggle;
 
-    void Start()
+    public void SetP1Color(int index)
     {
-        soundToggle.isOn = Instance.isSoundOn;
-        soundToggle.onValueChanged.AddListener(OnSoundToggleChanged);
+        p1Color = (PlayerColor)index;
     }
 
-    void OnSoundToggleChanged(bool value)
+    public void SetP2Color(int index)
     {
-        Instance.isSoundOn = value;
+        p2Color = (PlayerColor)index;
     }
+
+    public Color GetColor(PlayerColor color)
+    {
+        return color switch
+        {
+            PlayerColor.Red => Color.red,
+            PlayerColor.Green => Color.green,
+            PlayerColor.Blue => Color.blue,
+            PlayerColor.Yellow => Color.yellow,
+            _ => Color.red
+        };
+    }
+
+    public Color GetP1Color() => GetColor(p1Color);
+    public Color GetP2Color() => GetColor(p2Color);
 }
