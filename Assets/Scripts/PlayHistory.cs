3da292ab654f8e6a46874fd45bcda0ba2390414c
Artori4z <kongphop.buch@bumail.net>
2025-05-14 00:39:31 +0700

    .

--- Assets/Scripts/PlayHistory.cs
+++ Assets/Scripts/PlayHistory.cs
@@ -3,47 +3,30 @@ using UnityEngine;
 
 public class PlayHistory : MonoBehaviour
 {
-    int POneHighSCoinSample = 0;
-    int PTwoHighSCoinSample = 0;
-    float POneHighSTimeSample = 0;
-    float PTwoHighSTimeSample = 0;
-    [SerializeField] TextMeshProUGUI POneHighSCoinText;
-    [SerializeField] TextMeshProUGUI PTwoHighSCoinText;
-    [SerializeField] TextMeshProUGUI POneHighSTimeText;
-    [SerializeField] TextMeshProUGUI PTwoHighSTimeText;
+    int HighSCoinSample = 0;
+    float HighSTimeSample = 0;
+    [SerializeField] TextMeshProUGUI HighSCoinText;
+    [SerializeField] TextMeshProUGUI HighSTimeText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        POneHighSCoinSample = 8;// PlayerPrefs.GetInt("Coin", 0);
-        PTwoHighSCoinSample = 8;
-        POneHighSTimeSample = PlayerPrefs.GetFloat("Highscore", 0f);
-        PTwoHighSTimeSample = 4;
+        HighSCoinSample = 8;// PlayerPrefs.GetInt("Coin", 0);
+        HighSTimeSample = PlayerPrefs.GetFloat("Highscore", 0f);
         UpdateText();
     }
     void UpdateText()
     {
         //HightScore
         //Player1
-        if (POneHighSTimeSample <= 0)
+        if (HighSTimeSample <= 0)
         {
-            POneHighSCoinText.text = "Coin : - ";
-            POneHighSTimeText.text = "Time : - sec.";
+            HighSCoinText.text = "Coin : - ";
+            HighSTimeText.text = "Time : - sec.";
         }
         else
         {
-            POneHighSCoinText.text = $"Coin : {POneHighSCoinSample.ToString()}";
-            POneHighSTimeText.text = $"Time : {POneHighSTimeSample.ToString("F2")} sec.";
-        }
-        //Player2
-        if (PTwoHighSTimeSample <= 0)
-        {
-            PTwoHighSCoinText.text = "Coin : - ";
-            PTwoHighSTimeText.text = "Time : - sec.";
-        }
-        else
-        {
-            PTwoHighSCoinText.text = $"Coin : {PTwoHighSCoinSample.ToString()}";
-            PTwoHighSTimeText.text = $"Time : {PTwoHighSTimeSample.ToString("F2")} sec.";
+            HighSCoinText.text = $"Coin : {HighSCoinSample.ToString()}";
+            HighSTimeText.text = $"Time : {HighSTimeSample.ToString("F2")} sec.";
         }
     }
 }
